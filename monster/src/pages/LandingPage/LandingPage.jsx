1c4387707aff033b4814123ab2bc90f2048ff4b5
Zach <djessitokoto@gmail.com>
2023-09-30 22:43:05 +0200

    init router

---
+++ monster/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+// import react from 'react';
+
+
+const LandingPage = () => {
+    return (
+        <div>
+            Monster High
+        </div>
+    )
+};
+
+export default LandingPage;
---
+++ monster/src/routes/Router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import LandingPage from '../pages/LandingPage/LandingPage';
+
+import React from "react";
+
+     
+
+    
+export const router = createBrowserRouter ([
+    {
+        path: "/",
+        element: <LandingPage />,
+    }
+
+  
+]);
+  
